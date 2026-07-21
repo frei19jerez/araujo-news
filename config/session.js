08c6ac9ea672156cecdb98901e9d06aaabf46166
Freimel Jerez <frei19jerez@gmail.com>
2026-07-21 15:10:54 -0500

    Mejora del runtime: navegación con X-Runtime-Prefix y aislamiento de sesiones

--- config/session.js
+++ config/session.js
@@ -5,10 +5,14 @@
 
 module.exports.session = {
 
-  secret: process.env.SESSION_SECRET || '8b891772b17ab98752fab48df4e1f603',
+  secret:
+    process.env.SESSION_SECRET ||
+    '8b891772b17ab98752fab48df4e1f603',
+
+  name: 'araujo-news.sid',
 
   cookie: {
-    secure: false,
+    secure: process.env.NODE_ENV === 'production',
     httpOnly: true,
     sameSite: 'lax',
     maxAge: 1000 * 60 * 60 * 24 * 7
